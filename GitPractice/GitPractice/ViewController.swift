318897d2cbf43e0369ac3082d67c7a62d1d3b91c
Joshua <Rambo@ssa-1014.local>
2017-07-28 17:57:27 -0700

    Added new project

---
+++ GitPractice/GitPractice/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by Joshua on 7/28/17.
+//  Copyright © 2017 Joshua's Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //test
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
